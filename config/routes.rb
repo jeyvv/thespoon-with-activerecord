6415f3a0d298ee3e0a542c96b61ab2982f297ed5
Jey Val <jeremv@gmail.com>
2020-06-15 17:18:00 +0200

    final lecture update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "restaurants", to: "restaurants#index"
+
+ resources :restaurants
+
+  # # Lire tous les restaurants
+  # get 'restaurants', to: 'restaurants#index'
+
+  # # Lire un restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # # Create a restaurant (2 requests)
+  # # The `new` route needs to be *before* `show` route.
+  # get 'restaurants/new', to: 'restaurants#new' # affiche le formulaire
+  # post 'restaurants', to: 'restaurants#create' # enregistre le formulaire en db
+
+  # # Update a restaurant (2 requests)
+  # # Affiche form prerempli pr editer l'enregistrement
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # # Recupere les donnees saisies pour les modifier en db
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # # supprimer un restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: "Ganiere", rating: 2, address: "Champs Elysees")
+Restaurant.create(name: "La Serre", rating: 5, address: "Quai de la Rapée")
+Restaurant.create(name: "Le Dindon", rating: 3, address: "Rue Beautreillis")
